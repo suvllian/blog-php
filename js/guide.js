6b529b57cdd1dd44e72dbf1f5115fec2d1a0c496
Suvllian <suvlliansong@163.com>
2016-11-16 13:58:30 +0800

    首页添加动效

--- js/guide.js
+++ js/guide.js
@@ -28,6 +28,34 @@ window.onload = function(){
 	var i = 0;
 	console.log(length);
 
+	var WindowWidth = window.innerWidth;
+	var WindowHeight = window.innerHeight;
+
+	if (typeof WindowWidth != "number") {
+		//确定页面是否处于标准状态
+		if (document.compatMode == "CSS1Compat") {
+			WindowWidth = document.documentElement.clientWidth;
+			WindowHeight = document.documentElement.clientHeight;
+		}else{
+			WindowWidth = document.body.clientWidth;
+			WindowHeight = document.body.clientHeight;
+		}
+	}
+
+	var guide = document.getElementById("guide");
+	var content  = document.getElementById("content");
+	guide.style.height = WindowHeight+"px";
+	content.style.width = WindowWidth+"px";
+
+	//添加点击上浮效果
+	var btn = document.getElementById("toMain");
+	btn.onclick = function(){
+		guide.style.height = 0+"px";
+		guide.style.top = -WindowHeight+"px";
+		guide.style.transform = "translateY(-100%)";	
+		document.removeChild(guide);
+	}
+
 	setTimeout(animateDelay(h1),0);
 
 	var handle = setInterval(function(){
@@ -46,13 +74,13 @@ window.onload = function(){
 				top = top + 1;
 			}	
 			node.style.top = top+"px";
-		},120);
+		},100);
 		i++;
-	},500);
+	},300);
 
 	setTimeout(function(){
 		clearInterval(handle);
-	},4500);
+	},2400);
 
 	setTimeout(function(){
 		var top = 10;
@@ -65,11 +93,11 @@ window.onload = function(){
 		},200);
 
 		var handleTop = setInterval(function(){
-			if(top>-4){
+			if(top>0){
 				top = top - 1;
 			}	
 			(location[0]).style.top = top+"px";
 		},50);
-	},4000);
+	},1600);
 }
 
